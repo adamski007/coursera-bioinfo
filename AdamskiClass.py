70de6c4ff3fa58e966e454be11d8de6b12b591d0
Adam Szczepanski <spoown@gmail.com>
2013-11-12 20:03:23 +0100

    find all occurence integrated in the class

--- AdamskiClass.py
+++ AdamskiClass.py
@@ -4,6 +4,7 @@ class AdamskiClass:
 
     def __init__(self, strGenome):
         self.genome = strGenome
+
     def give_complement(self, letter):
         if ( letter == 'A' ):
             return 'T'
@@ -13,8 +14,18 @@ class AdamskiClass:
             return 'G'
         elif ( letter == 'G' ):
             return 'C'
+
     def buildReverseComplement(self):
         complement = ''
         for char in self.genome:
             complement = complement + self.give_complement(char)
         return complement[::-1]
+
+    def findAllOcc(self,kmers):
+        listPosition = []
+        idx = 0
+        while idx <= ( len(self.genome) - len(kmers) ):
+            if ( kmers == self.genome[idx:idx+len(kmers)] ) :
+                listPosition.append(idx)
+            idx = idx + 1
+        return listPosition
