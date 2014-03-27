678a04a81ab243894db15728a85e0f8dd95754ef
adamski007 <spoown@gmail.com>
2014-03-27 22:19:48 +0100

    test 123

--- test.py
+++ test.py
@@ -9,9 +9,16 @@ from AdamskiClass import AdamskiClass
 sys.setrecursionlimit(20000)
 infile = open(sys.argv[1],'r')
 #infile = open("data.txt",'r')
+
 nucleotide = infile.readline()
 nucleotide = nucleotide.replace('\n','')
 genomeClass = AdamskiClass(nucleotide)
+
+
+dna_string = infile.readline()
+dna_string = dna_string.replace('\n','')
+dna = AdamskiClass(dna_string)
+print dna.buildReverseComplement()
 
 
 list_str = AdamskiClass.readAndBuildListFromFile('listDNA.txt')
