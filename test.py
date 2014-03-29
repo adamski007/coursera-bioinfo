b42b24e8700621e17c0762e390c2c84b8ef7dc76
adam szczepanski <spoown@gmail.com>
2014-03-29 08:25:29 +0100

    Adding section main to test.py in standart convention.

--- test.py
+++ test.py
@@ -6,22 +6,26 @@ import pprint
 import time
 from AdamskiClass import AdamskiClass
 
-sys.setrecursionlimit(20000)
-infile = open(sys.argv[1],'r')
-nucleotide = infile.readline()
-nucleotide = nucleotide.replace('\n','')
-genomeClass = AdamskiClass(nucleotide)
+def main()
+    sys.setrecursionlimit(20000)
+    infile = open(sys.argv[1],'r')
+    nucleotide = infile.readline()
+    nucleotide = nucleotide.replace('\n','')
+    genomeClass = AdamskiClass(nucleotide)
 
-dic_dna = AdamskiClass.load_fasta_files( sys.argv[1])
-gc_max = -1
-fasta_id_max_gc = ''
-for id_fasta in dic_dna.keys():
-    cur_gc = AdamskiClass.compute_GC_content(dic_dna[id_fasta])
-    if cur_gc > gc_max:
-        fasta_id_max_gc = id_fasta
-        gc_max = cur_gc
-print fasta_id_max_gc
-print gc_max
+    dic_dna = AdamskiClass.load_fasta_files( sys.argv[1])
+    gc_max = -1
+    fasta_id_max_gc = ''
+    for id_fasta in dic_dna.keys():
+        cur_gc = AdamskiClass.compute_GC_content(dic_dna[id_fasta])
+        if cur_gc > gc_max:
+            fasta_id_max_gc = id_fasta
+            gc_max = cur_gc
+    print fasta_id_max_gc
+    print gc_max
 
 
 list_str = AdamskiClass.readAndBuildListFromFile('listDNA.txt')
+
+if __name__ == "__main__":
+    main()
